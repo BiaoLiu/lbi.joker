1f31007d0951fa02cee5eb64f3b5d3324349cb90
BiaoLiu <452381072@qq.com>
2016-04-24 17:25:45 +0800

    lbi.joker

--- src/config.py
+++ src/config.py
@@ -2,16 +2,15 @@
 
 # 邮箱配置
 mail_host = 'smtp.qq.com'
-mail_user = '452381072@qq.com'
-mail_password = 'lbxunzhbupc2875'
+mail_user = 'xxxx@qq.com'
+mail_password = 'xxxxx'
 
 # 邮件接收人
-receivers=['liubiao2875@icloud.com']
-# receivers=['liubiao2875@icloud.com','1812390147@qq.com','275304016@qq.com']
+receivers=['xxxxx@icloud.com']
 
 # redis配置
 redis_config={
-    'host':'120.27.46.167',
+    'host':'xx.xx.xx.xx',
     'port':6379,
     'db':0
 }
